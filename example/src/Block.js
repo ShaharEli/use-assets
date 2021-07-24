58886f8ae60f04a58110235eea1c7ff220874968
Shahar Eliyahu <shahar.e3@gmail.com>
2021-07-24 17:34:04 +0300

    init

--- example/src/Block.js
+++ example/src/Block.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useAssets } from "use-assets";
 
 export default function Block({ title, data }) {
+  const { assets } = useAssets();
+  console.log(assets?.cow);
+  console.log(assets);
+
   return (
     <div>
       <h3>{title}</h3>
