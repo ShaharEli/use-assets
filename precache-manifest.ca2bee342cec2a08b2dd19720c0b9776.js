de251b881363fb7839902746ac9d8b79262143b9
Shahar Eliyahu <shahar.e3@gmail.com>
2021-07-24 16:09:49 +0300

    Updates

---
+++ precache-manifest.ca2bee342cec2a08b2dd19720c0b9776.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84162a03b88a130722326c2769bdbfdd",
+    "url": "/use-assets/index.html"
+  },
+  {
+    "revision": "4d004fb4e64d7842549c",
+    "url": "/use-assets/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "732ac159d655922444d4",
+    "url": "/use-assets/static/js/2.a1994ccc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-assets/static/js/2.a1994ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0328da07226fc6c799c4",
+    "url": "/use-assets/static/js/3.ed6ac0b6.chunk.js"
+  },
+  {
+    "revision": "4e5eb0ff85461ca58d81",
+    "url": "/use-assets/static/js/4.e65f87d1.chunk.js"
+  },
+  {
+    "revision": "8aac8257f1413a59786d",
+    "url": "/use-assets/static/js/5.d0236082.chunk.js"
+  },
+  {
+    "revision": "549123a85aef511ad7ac",
+    "url": "/use-assets/static/js/6.dca3b585.chunk.js"
+  },
+  {
+    "revision": "4d004fb4e64d7842549c",
+    "url": "/use-assets/static/js/main.2be9bbbc.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/use-assets/static/js/main.2be9bbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e917a42eb9d7e18eace",
+    "url": "/use-assets/static/js/runtime-main.1c3525e8.js"
+  }
+]);
